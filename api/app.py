371b3403333aa29d458e7ff031a64ddc7c6406e1
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-09-24 01:12:16 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request, jsonify
-import os
+import os  # <-- Ensure you've imported 'os'
 from dotenv import load_dotenv
 import fetchresults
 import youtube
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
+# Use the environment variable to get the OpenAI API Key
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+if not OPENAI_API_KEY:
+    raise ValueError("No OPENAI_API_KEY set for Flask application")
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/process_video', methods=['POST'])
 def process_video():
@@ -28,7 +31,6 @@ def process_video():
     formatted_start_end = [{'start': round(s), 'end': round(e)} for s, e in start_and_end_timing]
 
     return jsonify({"success": True, "shorts": formatted_start_end})
-
 
 if __name__ == "__main__":
     app.run()
