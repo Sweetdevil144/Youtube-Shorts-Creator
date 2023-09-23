424517d1e0f7b1dece0146bf9b10ef60d7f4ff5c
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-09-24 01:14:15 +0530

    Update fetchresults.py

--- api/fetchresults.py
+++ api/fetchresults.py
@@ -6,7 +6,8 @@ import os
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-openai.api_key = OPENAI_API_KEY
+if not OPENAI_API_KEY:
+    raise ValueError("No OPENAI_API_KEY set for fetchresults")
 
 
 async def analyze_captions(text):
